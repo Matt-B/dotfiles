b974f9afd00d4937bd69db561f82fb0bbd8cc771
Matt Bailey <matt@thebaileyresidence.co.uk>
2014-06-26 15:05:46 +0100

    Added battery widget to awesome config.

--- rc.lua
+++ rc.lua
@@ -179,6 +179,19 @@ for s = 1, screen.count() do
     -- Create a tasklist widget
     mytasklist[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, mytasklist.buttons)
 
+    -- Create a battery widget
+    batterywidget = wibox.widget.textbox()    
+    batterywidget:set_text(" | Battery | ")    
+    batterywidgettimer = timer({ timeout = 5 })    
+    batterywidgettimer:connect_signal("timeout",    
+      function()    
+        fh = assert(io.popen("acpi | cut -d, -f 2,3 -", "r"))    
+        batterywidget:set_text(" |" .. fh:read("*l") .. " | ")    
+        fh:close()    
+      end    
+    )    
+    batterywidgettimer:start()
+
     -- Create the wibox
     mywibox[s] = awful.wibox({ position = "top", screen = s })
 
@@ -191,6 +204,7 @@ for s = 1, screen.count() do
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
     if s == 1 then right_layout:add(wibox.widget.systray()) end
+    right_layout:add(batterywidget)
     right_layout:add(mytextclock)
     right_layout:add(mylayoutbox[s])
 
