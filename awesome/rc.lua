0405682386285860e417244ce170ebc303e84729
Matt Bailey <matt@thebaileyresidence.co.uk>
2014-07-01 22:19:40 +0100

    Add reboot and shutdown items to awesome menu.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -97,7 +97,9 @@ myawesomemenu = {
 }
 
 mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-                                    { "open terminal", terminal }
+                                    { "open terminal", terminal },
+                                    { "reboot", "systemctl reboot" },
+                                    { "shutdown", "systemctl poweroff" }
                                   }
                         })
 
