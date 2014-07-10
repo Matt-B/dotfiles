4ef39f182b3f7b9d52b55e8abca046bbc9d96622
Matt Bailey <matthew.bailey@7digital.com>
2014-07-10 16:22:52 +0100

    Add xscreensaver keybinding to awesome and start it using xinitrc.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -311,7 +311,8 @@ globalkeys = awful.util.table.join(
                   awful.util.getdir("cache") .. "/history_eval")
               end),
     -- Menubar
-    awful.key({ modkey }, "p", function() menubar.show() end)
+    awful.key({ modkey }, "p", function() menubar.show() end),
+    awful.key({ modkey }, "l", function () awful.util.spawn("xscreensaver-command --lock") end)    
 )
 
 clientkeys = awful.util.table.join(
