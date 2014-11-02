093d58e119b90431833b076c4fae92c3738d1e0d
Matt <matt@thebaileyresidence.co.uk>
2014-11-02 10:40:28 +0000

    Remove some default menu options that I don't use from Awesome.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -89,17 +89,8 @@ for s = 1, screen.count() do
 end
 -- }}}
 
--- {{{ Menu
--- Create a laucher widget and a main menu
-myawesomemenu = {
-   { "manual", terminal .. " -e man awesome" },
-   { "edit config", editor_cmd .. " " .. awesome.conffile },
-   { "restart", awesome.restart },
-   { "quit", awesome.quit }
-}
-
-mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
-                                    { "open terminal", terminal },
+-- Menu
+mymainmenu = awful.menu({ items = { { "open terminal", terminal },
                                     { "reboot", "systemctl reboot" },
                                     { "shutdown", "systemctl poweroff" }
                                   }
